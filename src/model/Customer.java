8e3a0b94940a78357b930f12df7cdc44e0fdbe3e
FX553V <zahraabiri816@gmail.com>
2021-10-17 19:50:39 +0330

    add  customer property

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Customer {
+    private int id;
+    private String name;
+    private String family;
+    private String username;
+    private String password;
+    private int age;
+    private boolean buyStatus;
+
 }
